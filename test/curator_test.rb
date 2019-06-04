242da9db7b886c38390f5593ce981341e8c80587
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-04 11:48:01 -0600

    Add test for finding artists age when photograph was taken

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -266,4 +266,12 @@ class CuratorTest < Minitest::Test
     assert_equal [photo_1, photo_4], @curator.photographs_taken_between(1950..1965)
   end
 
+  def test_find_artists_photographs_by_age
+    @curator.load_photographs('./data/photographs.csv')
+    @curator.load_artists('./data/artists.csv')
+
+    diane_arbus = @curator.find_artist_by_id("3")
+    assert_equal ({44=>"Identical Twins, Roselle, New Jersey", 39=>"Child with Toy Hand Grenade in Central Park"}), @curator.artists_photographs_by_age(diane_arbus)
+  end
+
 end
