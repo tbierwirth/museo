fca20a06b5544c8b08a0c8a7fef34cad7304ae9b
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-04 09:57:07 -0600

    Add test to find photographs by artist

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -22,6 +22,19 @@ class CuratorTest < Minitest::Test
       artist_id: "2",
       year: "1941"
     }
+    photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+
+    photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
 
     artist_1 = {
       id: "1",
@@ -39,10 +52,21 @@ class CuratorTest < Minitest::Test
       country: "United States"
     }
 
+    artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    }
+
     @photo_1 = Photograph.new(photo_1)
     @photo_2 = Photograph.new(photo_2)
+    @photo_3 = Photograph.new(photo_3)
+    @photo_4 = Photograph.new(photo_4)
     @artist_1 = Artist.new(artist_1)
     @artist_2 = Artist.new(artist_2)
+    @artist_3 = Artist.new(artist_3)
   end
 
   def test_it_exists
@@ -79,6 +103,21 @@ class CuratorTest < Minitest::Test
 
     assert_equal @artist_1, @curator.find_artist_by_id("1")
     assert_equal @photo_2, @curator.find_photo_by_id("2")
+  end
+
+  def test_find_photographs_by_artist
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+
+    diane_arbus = @curator.find_artist_by_id("3")
+    # assert_equal @artist_3, @curator.find_artist_by_id("3")
+    assert_equal [@photo_3, @photo_4], @curator.find_photographs_by_artist(diane_arbus)
   end
 
 end
