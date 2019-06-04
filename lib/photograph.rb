a698f3fdcfe9c2819b72ac9b3161b6849e803134
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-04 09:12:29 -0600

    Fix typos

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -2,10 +2,10 @@ class Photograph
   attr_reader :id, :name, :artist_id, :year
 
   def initialize(info)
-    @id = info[id]
-    @name = info[name]
-    @artist_id = info[artist_id]
-    @year = info[year]
+    @id = info[:id]
+    @name = info[:name]
+    @artist_id = info[:artist_id]
+    @year = info[:year]
   end
 
 end
