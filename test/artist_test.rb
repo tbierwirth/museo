7c9dac9ddc8aa9609301f5e6176bbf0e4c4728ed
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-04 09:17:24 -0600

    Add test for attributes

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -15,4 +15,16 @@ class ArtistTest < Minitest::Test
     @artist = Artist.new(attributes)
   end
 
+  def test_it_exists
+    assert_instance_of Artist, @artist
+  end
+
+  def test_the_attributes
+    assert_equal "2", @artist.id
+    assert_equal "Ansel Adams", @artist.name
+    assert_equal "1902", @artist.born
+    assert_equal "1984", @artist.died
+    assert_equal "United States", @artist.country
+  end
+
 end
