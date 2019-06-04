cfd96ffb08c9598cc2af015824e84db9621f1d5c
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-04 09:43:52 -0600

    Add test to find photo by id

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -74,7 +74,11 @@ class CuratorTest < Minitest::Test
   def test_find_by_id
     @curator.add_artist(@artist_1)
     @curator.add_artist(@artist_2)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+
     assert_equal @artist_1, @curator.find_artist_by_id("1")
+    assert_equal @photo_2, @curator.find_photo_by_id("2")
   end
 
 end
