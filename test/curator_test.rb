9e47f5afb46ce82188b95c44a51699c8cd4c6d7e
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-04 09:37:54 -0600

    Add test for adding artists

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -15,22 +15,41 @@ class CuratorTest < Minitest::Test
       artist_id: "1",
       year: "1954"
     }
+
     photo_2 = {
       id: "2",
       name: "Moonrise, Hernandez",
       artist_id: "2",
       year: "1941"
     }
+
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+
     @photo_1 = Photograph.new(photo_1)
     @photo_2 = Photograph.new(photo_2)
+    @artist_1 = Artist.new(artist_1)
+    @artist_2 = Artist.new(artist_2)
   end
 
   def test_it_exists
     assert_instance_of Curator, @curator
   end
 
-  def test_add_photos_and_artists
-    assert_equal [], @curator.artists
+  def test_add_photos
     assert_equal [], @curator.photographs
 
     @curator.add_photograph(@photo_1)
@@ -41,4 +60,12 @@ class CuratorTest < Minitest::Test
     assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", @curator.photographs.first.name
   end
 
+  def test_add_artists
+    assert_equal [], @curator.artists
+
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+
+    assert_equal [@artist_1, @artist_2], @curator.artists
+  end
 end
